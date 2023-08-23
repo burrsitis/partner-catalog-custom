d6a47254fb371cbee9fd043edbf7358066493d3b
Burr Sutter <burrsutter@gmail.com>
2023-08-23 10:09:21 -0400

    saying Aloha

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -43,7 +43,7 @@ public class DemoApplication {
       String stringNow = simpleDateFormat.format(new Date());
 
       System.out.println("Aloha: " + cnt);
-      return "Bonjour Spring " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+      return "Aloha Spring " + cnt++ + " " + stringNow + " on " + HOSTNAME;
     }
 
     public static void main(String[] args) {
